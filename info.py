ba8b7729e0340cba04ead95a96c1a84e0f9ff48a
rockymiss <rachelmcdermottrock@gmail.com>
2022-04-03 11:57:26 +0000

    Move game choice and choices functions into info file, update imports after move and update main quiz function in run.py

--- info.py
+++ info.py
@@ -4,6 +4,13 @@ Information Messages for users
 
 import time
 from pyfiglet import figlet_format
+from colorama import Fore
+from quest import eng_question_list
+from quest import ire_question_list
+from quest import wales_question_list
+from quest import france_question_list
+from quest import scot_question_list
+from quest import italy_question_list
 
 
 def welcome_message():
@@ -18,7 +25,8 @@ def welcome_message():
     print('There are six sections in total, one for each country')
     time.sleep(2)
     user_name = input('What is your name player?: \n')
-    print(f'Hello {user_name} would you like to see the rules or go ahead and play the game?\n')
+    print(f'Hello {user_name} would you like to see the rules or\
+    go ahead and play the game?\n')
 
 
 def rules():
@@ -41,4 +49,65 @@ def rules():
     time.sleep(2)
     print("Let's see what country you cheer for...")
     time.sleep(2)
-    
+
+
+def game_choice():
+    """
+    This function gets the users choice on which game they want to play
+    """
+    get_choice = input("So what's it going to be?: \n")
+    if get_choice == "eng":
+        get_questions(eng_question_list)
+    elif get_choice == "ire":
+        get_questions(ire_question_list)
+    elif get_choice == "wal":
+        get_questions(wales_question_list)
+    elif get_choice == "fr":
+        get_questions(france_question_list)
+    elif get_choice == "sc":
+        get_questions(scot_question_list)
+    elif get_choice == "it":
+        get_questions(italy_question_list)
+    else:
+        print('You must choose a valid selection, please choose again...\n')
+        game_choice()
+
+
+def choices():
+    """
+    Function to display choices for user
+    """
+    time.sleep(2)
+    print('You have the choice of six quiz sections to play:')
+    time.sleep(1)
+    print(f'To answer questions about England type {Fore.RED}"eng"\
+    {Fore.WHITE}')
+    time.sleep(1)
+    print(f'To answer questions about Ireland type {Fore.GREEN}"ire"\
+    {Fore.WHITE}')
+    time.sleep(1)
+    print(f'To answer questions about Scotland type {Fore.BLUE}"sc"\
+    {Fore.WHITE}')
+    time.sleep(1)
+    print(f'To answer questions about Wales type {Fore.LIGHTRED_EX}\
+    "wal"{Fore.WHITE}')
+    time.sleep(1)
+    print(f'To answer questions about France type {Fore.BLUE}"fr"\
+    {Fore.WHITE}')
+    time.sleep(1)
+    print(f'To answer questions about Italy type {Fore.CYAN}"it"\
+    {Fore.WHITE}')
+    time.sleep(2)
+
+
+def get_questions(questions):
+    """
+    This function will get the questions for each section and get user
+    scores
+    """
+    score = 0
+    for question in questions:
+        answer = input(question.question)
+        if answer == question.answer:
+            score += 1
+    print(f' your score is {score} out of {questions}')
--- run.py
+++ run.py
@@ -1,16 +1,10 @@
 """True or False Quiz Game"""
 import gspread
-import time
-from colorama import Fore
+from google.oauth2.service_account import Credentials
 from info import welcome_message
 from info import rules
-from google.oauth2.service_account import Credentials
-from quest import eng_question_list
-from quest import ire_question_list
-from quest import wales_question_list
-from quest import france_question_list
-from quest import scot_question_list
-from quest import italy_question_list
+from info import choices
+from info import game_choice
 
 
 SCOPE = [
@@ -25,55 +19,6 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('true_false')
 
 data = SHEET.worksheet('scores')
-
-
-def get_questions(questions):
-    """
-    This function will get the questions for each section and get user
-    scores
-    """
-    score = 0
-    for question in questions:
-        answer = input(question.question)
-        if answer == question.answer:
-            score += 1
-    print(f"You got " + str(score) + '/' + str(len(questions)) + " correct")
-
-
-def game_choice():
-    """
-    This function gets the users choice on which game they want to play
-    """
-    print('You have the choice of six quiz sections to play:')
-    time.sleep(1)
-    print(f'To answer questions about England type {Fore.RED}"eng"{Fore.WHITE}')
-    time.sleep(1)
-    print(f'To answer questions about Ireland type {Fore.GREEN}"ire"{Fore.WHITE}')
-    time.sleep(1)
-    print(f'To answer questions about Scotland type {Fore.BLUE}"sc"{Fore.WHITE}')
-    time.sleep(1)
-    print(f'To answer questions about Wales type {Fore.LIGHTRED_EX}"wal"{Fore.WHITE}')
-    time.sleep(1)
-    print(f'To answer questions about France type {Fore.BLUE}"fr"{Fore.WHITE}')
-    time.sleep(1)
-    print(f'To answer questions about Italy type {Fore.CYAN}"it"{Fore.WHITE}')
-    time.sleep(2)
-    get_choice = input("So what's it going to be?: \n")
-    if get_choice == "eng":
-        get_questions(eng_question_list)
-    elif get_choice == "ire":
-        get_questions(ire_question_list)
-    elif get_choice == "wal":
-        get_questions(wales_question_list)
-    elif get_choice == "fr":
-        get_questions(france_question_list)
-    elif get_choice == "sc":
-        get_questions(scot_question_list)
-    elif get_choice == "it":
-        get_questions(italy_question_list)
-    else:
-        print('You must choose a valid selection, please choose again...\n')
-        game_choice()
 
 
 # def validate_choice(choices):
@@ -103,10 +48,12 @@ def main_quiz_start():
         rules()
         player_choice = input('Type p to play or q to quit: \n')
         if player_choice == 'p':
+            choices()
             game_choice()
         if player_choice == 'q':
             print('ok bye')
     elif player_choice == 'p':
+        choices()
         game_choice()
     else:
         print('You must enter a valid choice either "r" or "p"')
